2ef1fd9af7811a40f58f73bcf29e2ca1f319b5f0
deddyps <haicoders12@gmail.com>
2023-09-08 16:09:16 +0700

    fix several feedback

--- components/Thankyou/HeadBodyTable.js
+++ components/Thankyou/HeadBodyTable.js
@@ -10,12 +10,11 @@ export const RenderHeaderTable = () => {
     </div>
   );
 };
-export const RenderBodyTable = ({ data, paginatedData }) => {
+export const RenderBodyTable = ({ data }) => {
   const isModifierExist = data.modifiers;
   const isProductImageExist = data?.product?.defaultImageURL;
-
   return (
-    <div className='border-b border-gray-200 w-full grid grid-cols-19 p-[8px] text-[14px] font-normal lg:grid-cols-20 lg:text-[24px]'>
+    <div className='border-b border-gray-200 w-full grid grid-cols-19 pt-[8px] pb-[8px] text-[14px] font-normal lg:grid-cols-20 lg:text-[24px]'>
       <div>
         <div className='grid grid-cols-21 '>
           <div className='relative w-[36px] h-[36px] justify-self-center '>
@@ -25,7 +24,7 @@ export const RenderBodyTable = ({ data, paginatedData }) => {
               <Image src={defaultImage} alt='image' fill />
             )}
           </div>
-          <div>1x</div>
+          <div></div>
           <div>
             <table>
               <tbody>
@@ -54,7 +53,7 @@ export const RenderBodyTable = ({ data, paginatedData }) => {
             return (
               <div key={itemMod?.productID} className='grid grid-cols-21'>
                 <div></div>
-                <div>{itemMod?.quantity}x</div>
+                <div className='text-center'>{itemMod?.quantity}x</div>
                 <table>
                   <tbody>
                     <tr>
@@ -79,25 +78,17 @@ export const RenderBodyTable = ({ data, paginatedData }) => {
           });
         })}
       </div>
+
       <div className='flex flex-col justify-between'>
-        <div
-          className={`grid ${
-            paginatedData.length > 3 ? 'grid-cols-22' : 'grid-cols-23'
-          }`}
-        >
+        <div className='grid grid-cols-23'>
           <div className='justify-self-end'>$</div>
-          <div className='ml-1 justify-self-end'>{data?.nettAmount}</div>
+          <div className='ml-[2px] justify-self-end'>{data?.nettAmount}</div>
         </div>
         <div>
           {isModifierExist?.map((item) => {
             return item?.modifier?.details.map((itemMod) => {
               return (
-                <div
-                  key={itemMod?.productID}
-                  className={`grid ${
-                    paginatedData.length > 3 ? 'grid-cols-22' : 'grid-cols-23'
-                  }`}
-                >
+                <div key={itemMod?.productID} className='grid grid-cols-23'>
                   <div className='justify-self-end'>$</div>
                   <div className='justify-self-end'>{itemMod?.price}</div>
                 </div>
@@ -106,110 +97,9 @@ export const RenderBodyTable = ({ data, paginatedData }) => {
           })}
         </div>
       </div>
-      <div className='p-[8px] text-center'>{data?.quantity}</div>
+
+      <div className='text-center'>{data?.quantity}</div>
     </div>
   );
 };
 
-export const RenderBodyTables = ({ data }) => {
-  const isModifierExist = data.modifiers;
-  const isProductImageExist = data?.product?.defaultImageURL;
-  return (
-    <tbody className='bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900 text-[14px] lg:text-[24px]'>
-      <tr>
-        <td className='w-[469px] lg:w-[744px] p-[8px]'>
-          <div className='grid grid-cols-17 mt-6 '>
-            <div className='relative w-[36px] h-[36px] justify-self-center '>
-              {isProductImageExist ? (
-                <Image src={data?.product?.defaultImageURL} alt='image' fill />
-              ) : (
-                <Image src={defaultImage} alt='image' fill />
-              )}
-            </div>
-            <div className='text-center'>1x</div>
-            <div>
-              <table>
-                <tbody>
-                  <tr>
-                    <td
-                      style={{
-                        width: '100%',
-                        display: '-webkit-box',
-                        WebkitLineClamp: '2',
-                        WebkitBoxOrient: 'vertical',
-                        overflow: 'hidden',
-                        padding: 0,
-                        margin: 0,
-                      }}
-                    >
-                      {data?.product?.name}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          {isModifierExist?.map((item) => {
-            return item?.modifier?.details.map((itemMod) => {
-              return (
-                <div key={itemMod?.productID} className='grid grid-cols-17'>
-                  <div></div>
-                  <div className='justify-self-center'>
-                    {itemMod?.quantity}x
-                  </div>
-
-                  <table>
-                    <tbody>
-                      <tr>
-                        <td
-                          style={{
-                            width: '100%',
-                            display: '-webkit-box',
-                            WebkitLineClamp: '2',
-                            WebkitBoxOrient: 'vertical',
-                            overflow: 'hidden',
-                            padding: 0,
-                            margin: 0,
-                          }}
-                        >
-                          {itemMod?.name}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              );
-            });
-          })}
-        </td>
-
-        <td className='w-[96px] lg:w-[128px] text-center relative p-[8px]'>
-          <div className='absolute top-6 left-9 '>$ {data?.nettAmount}</div>
-          <div className='absolute bottom-0 left-9'>
-            {isModifierExist?.map((item) => {
-              return item?.modifier?.details.map((itemMod) => {
-                return <div key={itemMod?.productID}>$ {itemMod?.price}</div>;
-              });
-            })}
-          </div>
-        </td>
-        <td className='w-[96px] lg:w-[128px] text-center relative p-[8px]'>
-          <div className='absolute top-6 left-12'>{data?.quantity}</div>
-          <div className='text-transparent mt-5'>
-            <div className='mt-5'>
-              {isModifierExist?.map((item) => {
-                return item?.modifier?.details.map((itemMod) => {
-                  return (
-                    <div className='text-transparent' key={itemMod?.productID}>
-                      $ {itemMod?.price}
-                    </div>
-                  );
-                });
-              })}
-            </div>
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  );
-};
